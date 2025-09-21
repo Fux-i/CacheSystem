8693feacf261598f22318922d4554b0134aa7c91
fuxi <1538130391@qq.com>
2025-09-21 21:05:39 +0800

    ✨feat: 新增计时器工具

---
+++ src/utils/timer.cpp
@@ -0,0 +1,16 @@
+#include "timer.h"
+#include <iostream>
+
+Timer::Timer(std::string name) : name_(std::move(name))
+{
+    start_ = std::chrono::high_resolution_clock::now();
+}
+
+Timer::~Timer()
+{
+    using duration = std::chrono::duration<double>;
+    time_point end = std::chrono::high_resolution_clock::now();
+    duration   dur = end - start_;
+    std::cout << "{Timer}"
+              << "[" << name_ << "] took " << dur.count() << " seconds\n";
+}
---
+++ src/utils/timer.h
@@ -0,0 +1,14 @@
+#include <chrono>
+#include <string>
+
+class Timer
+{
+  public:
+    Timer(std::string name);
+    ~Timer();
+
+  private:
+    using time_point = std::chrono::time_point<std::chrono::high_resolution_clock>;
+    std::string name_;
+    time_point  start_;
+};
