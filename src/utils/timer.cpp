9d3249f09b48ef5e1cbb7147929e7b59a9377386
fuxi <1538130391@qq.com>
2025-09-27 15:48:19 +0800

    ✨feat: 使 timer 工具更加灵活

--- src/utils/timer.cpp
+++ src/utils/timer.cpp
@@ -1,16 +1,27 @@
 #include "timer.hpp"
 #include <iostream>
 
-Timer::Timer(std::string name) : name_(std::move(name))
+Timer::Timer(std::string name, bool autoStop) : name_(std::move(name)), autoStop_(autoStop)
 {
     start_ = std::chrono::high_resolution_clock::now();
 }
 
 Timer::~Timer()
 {
-    using duration = std::chrono::duration<double>;
+    if (autoStop_)
+        return;
+    using duration = std::chrono::duration<double, std::milli>;
     time_point end = std::chrono::high_resolution_clock::now();
     duration   dur = end - start_;
     std::cout << "{Timer}"
-              << "[" << name_ << "] took " << dur.count() << " seconds\n";
+              << "[" << name_ << "] took " << dur.count() << " ms\n";
 }
+
+double Timer::getElapsedMilliseconds() const
+{
+    time_point end      = std::chrono::high_resolution_clock::now();
+    auto       duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start_);
+    return static_cast<double>(duration.count());
+}
+
+void Timer::restart() { start_ = std::chrono::high_resolution_clock::now(); }
--- src/utils/timer.hpp
+++ src/utils/timer.hpp
@@ -6,11 +6,17 @@
 class Timer
 {
   public:
-    Timer(std::string name);
+    Timer(std::string name, bool autoStop = false);
     ~Timer();
+
+    // 获取已经过的毫秒数（不结束计时）
+    double getElapsedMilliseconds() const;
+
+    void restart();
 
   private:
     using time_point = std::chrono::time_point<std::chrono::high_resolution_clock>;
     std::string name_;
+    bool        autoStop_;
     time_point  start_;
 };
