ea22c1a41e9f79e08b13f630b12ee421e63094cc
fuxi <1538130391@qq.com>
2025-11-12 19:43:51 +0800

    🦄refactor: Make it header-only

--- src/utils/timer.hpp
+++ src/utils/timer.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <chrono>
+#include <iostream>
 #include <string>
 
 class Timer
@@ -20,3 +21,27 @@ class Timer
     bool        autoStop_;
     time_point  start_;
 };
+
+inline Timer::Timer(std::string name, bool autoStop) : name_(std::move(name)), autoStop_(autoStop)
+{
+    start_ = std::chrono::high_resolution_clock::now();
+}
+
+inline Timer::~Timer()
+{
+    if (autoStop_)
+        return;
+    using duration = std::chrono::duration<double, std::milli>;
+    time_point end = std::chrono::high_resolution_clock::now();
+    duration   dur = end - start_;
+    std::cout << "{Timer}" << "[" << name_ << "] took " << dur.count() << " ms\n";
+}
+
+inline double Timer::getElapsedMilliseconds() const
+{
+    time_point end      = std::chrono::high_resolution_clock::now();
+    auto       duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start_);
+    return static_cast<double>(duration.count());
+}
+
+inline void Timer::restart() { start_ = std::chrono::high_resolution_clock::now(); }
